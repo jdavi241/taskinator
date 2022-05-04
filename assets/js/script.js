c6e8e41b815511b8d45241b32f4243a42636ea99
jdavi241 <davisjimm25@gmail.com>
2022-05-03 19:10:00 -0500

    JS buttons

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+window.document.querySelector("button");
+
+var btn = window.document.querySelector("button");
+console.dir(btn);
+
+var buttonEl = document.querySelector("#save-task");
+console.log(buttonEl);
+
