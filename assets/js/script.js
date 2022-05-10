bed94e3bb764e51391131fb89e25613700d71055
jdavi241 <davisjimm25@gmail.com>
2022-05-10 17:37:44 -0500

    empty form alert

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,12 @@ var taskFormHandler = function(event) {
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
-   
+
+   // Check if input values are empty strings 
+   if (!taskNameInput || !taskTypeInput) {
+       alert("You need to fill out the task form!");
+       return false;
+   }
     // package up data as an object 
     var taskDataObj = {
         name: taskNameInput,
